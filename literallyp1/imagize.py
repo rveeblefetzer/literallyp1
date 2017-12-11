4cbd3dcf9c3541d1f747b8fbd9b1a7cf1385f3e4
Rick Valenzuela <devnull@rickv.com>
2017-12-11 16:43:52 +0800

    adds line counting to ImageDraw; some refactoring

--- literallyp1/imagize.py
+++ literallyp1/imagize.py
@@ -33,16 +33,16 @@ def write_text_to_image():
     """Create and save image with other top stories."""
     lady_gray = (220, 220, 220)
     font_color = "#000"
-    left_padding = 10
     font = ImageFont.truetype("Domine-Regular.ttf", 62)
     line_height = font.getsize('text')[1]
-    num_lines = 35
-    image_height = line_height * (num_lines + 1)
+    with open('nyt_tweet_img.txt', 'r') as f:
+        num_lines = sum(1 for _ in f)
+    image_height = line_height * (num_lines * 1.75)
     image = Image.new("RGB", (2625, int(image_height)), lady_gray)
     draw = ImageDraw.Draw(image)
     with open('nyt_tweet_img.txt', 'r') as text:
-        draw.multiline_text((left_padding, 10), text.read(), font_color,
-                            font=font, spacing=35)
-    img_expanded = ImageOps.expand(image, border=30, fill=lady_gray)
-    img_resized = img_expanded.resize((500, int(image_height / 5.25)), Image.BICUBIC)
+        draw.multiline_text((100,100), text.read(), font_color,
+                             font=font, spacing=35)
+    img_resized = image.resize((500, int(image_height / 5.25)), Image.BICUBIC)
     img_resized.save("nyt_tweet.png")
+
